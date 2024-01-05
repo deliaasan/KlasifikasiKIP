4f5683b2470a885c98c9b115372cbb2a8399e14f
deliaasan <155346905+deliaasan@users.noreply.github.com>
2024-01-06 06:02:08 +0700

    Update view.py

--- view.py
+++ view.py
@@ -34,10 +34,10 @@ def main():
             st.write("Dataset Preview:")
             # st.write(df)
             try:
-                except FileNotFoundError as e:
+            except FileNotFoundError as e:
                 st.error(f"File tidak ditemukan: {e}")
-                except Exception as e:
-            st.error(f"Terjadi kesalahan: {e}")
+            except Exception as e:
+                st.error(f"Terjadi kesalahan: {e}")
             realFile = pd.read_csv(file_path)    
             realFile = pd.DataFrame(realFile)
             st.dataframe(realFile)
